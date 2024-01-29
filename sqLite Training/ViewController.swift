67f98cab297785733f6c8fd8309f75ad3670e87e
Pooyan J <pooyanjavidi@gmail.com>
2024-01-29 14:39:33 +0330

    Initial Commit

---
+++ sqLite Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sqLite Training
+//
+//  Created by Pooyan J on 11/9/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
